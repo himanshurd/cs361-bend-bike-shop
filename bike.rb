6437edafe8d1bf8d7b9713c39f726697bc74699e
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-11-14 19:18:35 -0800

    Update bike.rb

--- bike.rb
+++ bike.rb
@@ -5,19 +5,14 @@ class Bike
   STANDARD_WEIGHT = 200 # lbs
   MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :cargo_contents
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
     @cargo_contents = []
-  end
-
-  def rent!
-    self.rented = true
   end
 
   def add_cargo(item)
